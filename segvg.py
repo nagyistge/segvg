c9217dea7d52221ac486945731b76668aa47c38e
vsoch <vsochat@stanford.edu>
2015-12-29 12:33:37 -0800

    adding first rendering of brain regions on canvas

--- segvg.py
+++ segvg.py
@@ -2,32 +2,37 @@
 import nibabel
 import sys
 import cv2
+from scipy.spatial.distance import pdist
+import numpy
 
-mr = sys.argv[1]
+#mr = sys.argv[1]
 
-mr = "brain_parcellation_mcinet_basc_asym_111clusters.nii.gz"
+mr = "brain_parcellation_mcinet_basc_asym_33clusters.nii.gz"
 nii = nibabel.load(mr)
 
-# Take some random slice (could be user selected)
-slice = nii.get_data()[:,:,20]
+# We will squash the image from the top, for each unique value
+regions = numpy.unique(nii.get_data()).tolist()
 
-# Can we convert numpy array to grayscale?
-im = numpy.array(slice * 255, dtype = numpy.uint8)  # Note - will need to save mapping here from labels to new values
-threshed = cv2.adaptiveThreshold(im, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3, 0)
-#gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-edged = cv2.Canny(im, 50, 100)
-cv2.imshow("Original", edged)
- 
-# find contours in the image
-(cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-mask = numpy.ones(slice.shape[:2], dtype="uint8") * 255
- 
-# loop over the contours
-for c in cnts:
-    # write me - output coordinates to interactive image
- 
-# remove the contours from the image and show the resulting images
-image = cv2.bitwise_and(image, image, mask=mask)
-cv2.imshow("Mask", mask)
-cv2.imshow("After", image)
-cv2.waitKey(0)
+# We don't want to contour value of 0 (nothingness)
+regions.pop(regions.index(0))
+
+# We will save a list of segments
+# needs to look like var segments = [{points:[[14, 44], [14, 46],[15, 46],[14, 45],[15, 44],[16, 44],[17, 45],[16, 44]]}]
+segments = []
+
+for value in regions:
+    empty = numpy.zeros(nii.shape)
+    empty[nii.get_data()==value] = value
+    squash = empty.sum(axis=0) # user can select view
+    squash[squash!=0] = 1      # binarize
+    # Can we convert numpy array to grayscale?
+    im = numpy.array(squash * 255, dtype = numpy.uint8)  # Note - will need to save mapping 
+    ret, thresh = cv2.threshold(im,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
+    
+    # Note: need way to separate right and left
+    
+    # loop over the contours
+    print len(cnts) # should always be 1 or 2 - the first entry is the external contour
+    new_segment = {"points":[x[0] for x in cnts[0].tolist()]}
+    segments.append(new_segment)
